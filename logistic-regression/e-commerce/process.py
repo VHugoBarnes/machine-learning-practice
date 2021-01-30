4ef9674d7919a1cac79b8dd59249ef6f9207bb09
Víctor Hugo Vázquez Gómez <hugovg799@gmail.com>
2021-01-30 17:28:39 -0600

    Making predictions

--- logistic-regression/e-commerce/process.py
+++ logistic-regression/e-commerce/process.py
@@ -2,8 +2,8 @@ import numpy as np
 import pandas as pd
 
 def get_data():
-    df = pd.read_csv('ecommerce_data.csv')
-    data = df.as_matrix()
+    df = pd.read_csv('./ecommerce_data.csv')
+    data = df.values
 
     X = data[:, :-1]
     Y = data[:, -1]
