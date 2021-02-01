1e92cc26fb350a1aff9312f68b8d513e539653c7
Víctor Hugo Vázquez Gómez <hugovg799@gmail.com>
2021-02-01 16:06:07 -0600

    Training the logistic model

--- logistic-regression/e-commerce/process.py
+++ logistic-regression/e-commerce/process.py
@@ -1,36 +1,70 @@
 import numpy as np
 import pandas as pd
+import os
+
+# so scripts from other folders can import this file
+dir_path = os.path.abspath(os.path.dirname(os.path.realpath(__file__)))
+
+# normalize numerical columns
+# one-hot categorical columns
 
 def get_data():
-    df = pd.read_csv('./ecommerce_data.csv')
-    data = df.values
+  df = pd.read_csv(dir_path + '/ecommerce_data.csv')
 
-    X = data[:, :-1]
-    Y = data[:, -1]
+  # just in case you're curious what's in it
+  # df.head()
 
-    # Normalice the numerical columns
-    X[:,1] = (X[:,1] - X[:, 1].mean()) / X[:,1].std()
-    X[:,2] = (X[:,2] - X[:, 2].mean()) / X[:,2].std()
+  # easier to work with numpy array
+  data = df.values
 
-    N, D = X.shape
-    X2 = np.zeros((N, D+3))
-    X2[:, 0:(D-1)] = X[:, 0:(D-1)]
+  # shuffle it
+  np.random.shuffle(data)
 
-    # One hot encoding
-    for n in range(N):
-        t = int(X[n, D-1])
-        X2[n, t+D-1] = 1
-    
-    Z = np.zeros((N, 4))
-    Z[np.arange(N), X[:, D-1].astype(np.int32)] = 1
-    # X2[:, -4:] = Z
-    assert(np.abs(X2[:, -4:] - Z).sum() < 10e-10)
+  # split features and labels
+  X = data[:,:-1]
+  Y = data[:,-1].astype(np.int32)
 
-    return X2, Y
+  # one-hot encode the categorical data
+  # create a new matrix X2 with the correct number of columns
+  N, D = X.shape
+  X2 = np.zeros((N, D+3))
+  X2[:,0:(D-1)] = X[:,0:(D-1)] # non-categorical
+
+  # one-hot
+  for n in range(N):
+      t = int(X[n,D-1])
+      X2[n,t+D-1] = 1
+
+  # method 2
+  # Z = np.zeros((N, 4))
+  # Z[np.arange(N), X[:,D-1].astype(np.int32)] = 1
+  # # assign: X2[:,-4:] = Z
+  # assert(np.abs(X2[:,-4:] - Z).sum() < 1e-10)
+
+  # assign X2 back to X, since we don't need original anymore
+  X = X2
+
+  # split train and test
+  Xtrain = X[:-100]
+  Ytrain = Y[:-100]
+  Xtest = X[-100:]
+  Ytest = Y[-100:]
+
+  # normalize columns 1 and 2
+  for i in (1, 2):
+    m = Xtrain[:,i].mean()
+    s = Xtrain[:,i].std()
+    Xtrain[:,i] = (Xtrain[:,i] - m) / s
+    Xtest[:,i] = (Xtest[:,i] - m) / s
+
+  return Xtrain, Ytrain, Xtest, Ytest
+
 
 def get_binary_data():
-    X, Y = get_data()
-    X2 = X[Y <= 1]
-    Y2 = Y[Y <= 1]
-
-    return X2, Y2
+  # return only the data from the first 2 classes
+  Xtrain, Ytrain, Xtest, Ytest = get_data()
+  X2train = Xtrain[Ytrain <= 1]
+  Y2train = Ytrain[Ytrain <= 1]
+  X2test = Xtest[Ytest <= 1]
+  Y2test = Ytest[Ytest <= 1]
+  return X2train, Y2train, X2test, Y2test
